5236418cefc329d010b0f63b667ee293b71cbaca
blushi <marie.gauthier63@gmail.com>
2020-04-20 16:38:09 +0200

    Change credits units types

---
+++ sql/V1_9__units_type.sql
@@ -0,0 +1,3 @@
+alter table credit_vintage alter column units type numeric;
+alter table account_balance alter column liquid_balance type numeric;
+alter table account_balance alter column burnt_balance type numeric;
