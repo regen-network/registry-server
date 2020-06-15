a732dc0d4643ad8be1c14b707187b34992cf0ea8
blushi <marie.gauthier63@gmail.com>
2020-06-15 17:01:18 +0200

    Back to ssl=true

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
 process.env.DATABASE_URL =
-  `${process.env.DATABASE_URL}?sslmode=verify-ca&sslrootcert=../config/rds-combined-ca-bundle.pem` ||
+  `${process.env.DATABASE_URL}?ssl=true&sslrootcert=../config/rds-combined-ca-bundle.pem` ||
   "postgres://postgres@localhost:5432/xrn";
