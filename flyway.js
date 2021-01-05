33857c37b6f22a708a6b5b9929a3037c4038bffc
blushi <marie.gauthier63@gmail.com>
2021-01-05 15:02:14 +0100

    Upgrade ts-node and wip on docker/flyway

--- flyway.js
+++ flyway.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return {
     flywayArgs: {
-      url: process.env.FLYWAY_URL || 'jdbc:postgresql:xrn',
+      url: process.env.FLYWAY_URL || 'jdbc:postgresql:regen_registry',
       locations: 'filesystem:sql',
       user: process.env.FLYWAY_USER || 'postgres',
       password: process.env.FLYWAY_PASSWORD || '',
