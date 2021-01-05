442947fa2a3fbb6ddec00845f9c24106724250e3
blushi <marie.gauthier63@gmail.com>
2021-01-05 11:44:47 +0100

    Fix issue_credits test

--- server/__tests__/db/functions/issue_credits.test.ts
+++ server/__tests__/db/functions/issue_credits.test.ts
@@ -105,7 +105,14 @@ it('issues credits with buffer pool and permanence reversal pool', () =>
     await becomeRoot(client);
     const project = await createProject(client, 'project name', party.wallet_id);
     expect(project).not.toBeNull();
-    // TODO create buffer pool and permanence rev pool users
+    await client.query(
+      `select really_create_user_if_needed('permanence-registry@regen.network',
+      'Permanence Reversal Buffer', null, null, '{"administrative", "buyer"}', '{}'::jsonb, 'permanence', false)`
+    );
+    await client.query(
+      `select really_create_user_if_needed('bufferpool-registry@regen.network',
+      'Buffer Pool', null, null, '{"administrative", "buyer"}', '{}'::jsonb, 'bufferpool', false)`
+    );
     await client.query(
       `insert into credit_class_version (id, name, version, date_developed, state_machine, metadata)
       values ($1, 'credit class name', 'v1.0', now(), '{}'::jsonb, $2)`,
@@ -119,13 +126,19 @@ it('issues credits with buffer pool and permanence reversal pool', () =>
     expect(result.issue_credits).not.toBeNull();
     expect(result.issue_credits.creditVintageId).not.toBeNull();
     expect(result.issue_credits.accountBalances).not.toBeNull();
-    expect(result.issue_credits.accountBalances).toHaveLength(2);
-    expect(result.issue_credits.accountBalances[0].name).toEqual('landSteward');
-    expect(result.issue_credits.accountBalances[0].percentage).toEqual(30);
-    expect(result.issue_credits.accountBalances[0].amount).toEqual(300);
-    expect(result.issue_credits.accountBalances[1].name).toEqual('projectDeveloper');
-    expect(result.issue_credits.accountBalances[1].percentage).toEqual(45);
-    expect(result.issue_credits.accountBalances[1].amount).toEqual(450);
+    expect(result.issue_credits.accountBalances).toHaveLength(4);
+    expect(result.issue_credits.accountBalances[0].name).toEqual('bufferPool');
+    expect(result.issue_credits.accountBalances[0].percentage).toEqual(20);
+    expect(result.issue_credits.accountBalances[0].amount).toEqual(200);
+    expect(result.issue_credits.accountBalances[1].name).toEqual('permanenceReversalBuffer');
+    expect(result.issue_credits.accountBalances[1].percentage).toEqual(5);
+    expect(result.issue_credits.accountBalances[1].amount).toEqual(50);
+    expect(result.issue_credits.accountBalances[2].name).toEqual('landSteward');
+    expect(result.issue_credits.accountBalances[2].percentage).toEqual(30);
+    expect(result.issue_credits.accountBalances[2].amount).toEqual(300);
+    expect(result.issue_credits.accountBalances[3].name).toEqual('projectDeveloper');
+    expect(result.issue_credits.accountBalances[3].percentage).toEqual(45);
+    expect(result.issue_credits.accountBalances[3].amount).toEqual(450);
 
     // credit vintage created
     const { rows: vintages } = await client.query(
@@ -168,6 +181,30 @@ it('issues credits with buffer pool and permanence reversal pool', () =>
     expect(balances[1].wallet_id).toEqual(stewardParties[0].wallet_id);
     expect(parseFloat(balances[1].liquid_balance)).toEqual(300);
     expect(parseFloat(balances[1].burnt_balance)).toEqual(0);
+
+    const { rows: permanenceParties } = await client.query(
+      `select wallet_id from party
+      inner join "user" on "user".email = 'permanence-registry@regen.network'
+      where party.id = "user".party_id`
+    );
+    expect(permanenceParties).toHaveLength(1);
+    expect(permanenceParties[0]).not.toBeNull();
+    expect(permanenceParties[0].wallet_id).not.toBeNull();
+    expect(balances[2].wallet_id).toEqual(permanenceParties[0].wallet_id);
+    expect(parseFloat(balances[2].liquid_balance)).toEqual(50);
+    expect(parseFloat(balances[2].burnt_balance)).toEqual(0);
+
+    const { rows: bufferParties } = await client.query(
+      `select wallet_id from party
+      inner join "user" on "user".email = 'bufferpool-registry@regen.network'
+      where party.id = "user".party_id`
+    );
+    expect(bufferParties).toHaveLength(1);
+    expect(bufferParties[0]).not.toBeNull();
+    expect(bufferParties[0].wallet_id).not.toBeNull();
+    expect(balances[3].wallet_id).toEqual(bufferParties[0].wallet_id);
+    expect(parseFloat(balances[3].liquid_balance)).toEqual(200);
+    expect(parseFloat(balances[3].burnt_balance)).toEqual(0);
   })
 );
 
