99a9cc90b39a811e6bfefff8ea421f7ac6d4635b
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-02-23 06:55:17 -0500

    recaptcha/verify

--- server/routes/recaptcha.ts
+++ server/routes/recaptcha.ts
@@ -1,15 +1,12 @@
 import * as express from 'express';
-const https = require('https')
 const axios = require('axios')
 import * as bodyParser from 'body-parser';
 
 const router = express.Router();
-
 const recaptchaKey = process.env.RECAPTCHA_SECRET_KEY;
 
-router.post('/recaptcha', bodyParser.json(), async (request, response: express.Response) => {
+router.post('/recaptcha/verify', bodyParser.json(), async (request, response: express.Response) => {
   const body = request.body;
-  console.log('***userResponse***',body.userResponse)
 
   axios
     .post(`https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaKey}&response=${body.userResponse}`, {
@@ -19,8 +16,6 @@ router.post('/recaptcha', bodyParser.json(), async (request, response: express.R
       },
     })
     .then(res => {
-      console.log(`##statusCode##: ${res.status}`)
-      console.log('##data##:',res.data)
       response.send(res.data);
     })
     .catch(error => {
