2eba27621cde713001f3466b52f41c75b69c7f3a
David Daniel <davedan7@gmail.com>
2021-04-09 18:04:36 -0600

    migrations & policy updates for user profile

--- sql/V1_59__user_org_party_update_perms.sql
+++ sql/V1_59__user_org_party_update_perms.sql
@@ -1,29 +1,13 @@
 create policy user_self_update on "user" for
 update using (auth0_sub = public.get_current_user());
 
--- create policy user_party_self_update on party for update
--- using (
---   party_id in (select party_id from "user" where auth0_sub = public.get_current_user() )
--- );
-
--- create policy user_party_self_update on party for update
--- using (
---   exists(
---     party_id in (select party_id from "user" where auth0_sub = public.get_current_user() )
---   )
--- );
--- create policy user_party_self_update on party for update
--- using (
---   exists(
---     select 1 from "user" where auth0_sub = public.get_current_user()
---     and party_id = party.id
---   )
--- );
+grant update (description, image, name) on party to app_user;
+grant update (website, legal_name) on organization to app_user;
 
 create policy user_party_self_update on party
   for update using (
     id in (select party_id from "user" where auth0_sub = public.get_current_user())
-   );
+  );
 
 create policy owner_update_organization on organization
   for update using (
